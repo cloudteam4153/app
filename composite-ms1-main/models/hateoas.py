5fbfa3779ae977bef8c89da595e5067088c085f4
Beverly <bq2159@columbia.edu>
2025-12-11 20:08:41 -0500

    Remove read-only composite ms

--- composite-ms1-main/models/hateoas.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class HATEOASLink(BaseModel):
-    rel: str          # "self", "update", "delete"
-    href: str           # absolute URL
-    method: str       # "GET", "POST", "PUT", "DELETE"
--- composite-ms1-main/security/__init__.py
+++
@@ -1,4 +0,0 @@
-from security.TokenCipher import TokenCipher
-from config.settings import settings
-
-token_cipher = TokenCipher(key=settings.TOKEN_ENCRYPTION_KEY)
